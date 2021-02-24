11449f1238df6ee22d34871d90e4f99a0f47a8c9
Matheus Prado <matheusfenoliodoprado@outlook.com>
2021-02-24 15:26:27 -0300

    Lambda

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
+'use strict'
+
 const express = require('express');
 
 const handler = express();
 
-handler.get('/test', async (req, res) => {
-    console.log(req.headers);
+handler.get('/test', async (req, res) => res.send(`Hello World!`, req.headers));
 
-    return formatResponse(req.headers);
-})
-
-const formatResponse = function(body){
-    var response = {
-      "statusCode": 200,
-      "headers": {
-        "Content-Type": "application/json"
-      },
-      "isBase64Encoded": false,
-      "multiValueHeaders": { 
-        "X-Custom-Header": ["My value", "My other value"],
-      },
-      "body": JSON.stringify(body)
-    }
-    return response
-  }
-
-  module.exports = handler;
+module.exports = handler;
