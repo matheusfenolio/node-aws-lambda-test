588698fdfe3808913a2af946a4e00ace41b3f263
Matheus Prado <matheusfenoliodoprado@outlook.com>
2021-02-25 10:21:47 -0300

    Add message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.get('/getIndividuals', (req, res) => {
 console.log('aaaaaaaaa', req.headers.lastname)
-res.send({ ...req.headers });
+res.status(200).json({ message: 'Hello World!' });
 });
 //app.listen(3000, () => console.log(`Listening on: 3000`));
 module.exports.handler = serverless(app);
